fc015eecfce96db0068f201c3f759a314a5c983a
Lucas Marques de Araujo <lucas.araujo@maua.br>
2018-05-28 10:48:36 -0300

    pratica 6

---
+++ pratica6/Compras.java
@@ -0,0 +1,17 @@
+package compras;
+
+import java.util.ArrayList;
+
+public class Compras {
+
+    public static void main(String[] args) {
+     ArrayList<String> arrList = new ArrayList<String>();
+       
+       arrList.add("Vai 1");
+       arrList.add("Vai 2");
+       arrList.add("Vai 3");
+       Integer a = 22;
+       System.out.println("arrList");
+    }
+    
+}
